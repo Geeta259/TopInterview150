80c3709f8b1299e7780ffd1f515f4dacc57fb542
Geeta <75520947+Geeta259@users.noreply.github.com>
2024-08-11 14:59:14 +0530

    Update and rename RemoveDuplicate2 to RemoveDuplicate2.java

--- Array/RemoveDuplicate2.java
+++ Array/RemoveDuplicate2.java
@@ -6,3 +6,15 @@ Output: 5, nums = [1,1,2,2,3,_]
 Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
 */
+
+ public int removeDuplicates(int[] nums) {
+        int i=0,j=1;
+        while(j<nums.length)
+        {
+            if(nums[j]==nums[i] && i>0 && nums[i]==nums[i-1])
+            { j++; continue; }
+
+            nums[++i] = nums[j++];
+        }
+        return i+1;
+    }
