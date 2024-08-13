a89c09dedfa0a78fdf1506bdba3dd56565b6c069
Geeta <75520947+Geeta259@users.noreply.github.com>
2024-08-13 17:53:58 +0530

    Create HIndex.java

---
+++ Array/HIndex.java
@@ -0,0 +1,7 @@
+/*
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+Input: citations = [3,0,6,1,5]
+Output: 3
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+*/
