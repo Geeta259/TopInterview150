44067edff9b08eadb29d911a0cf4352b09c7d7f2
Geeta <75520947+Geeta259@users.noreply.github.com>
2024-08-21 17:53:16 +0530

    Create SubStringConcatWords.java

---
+++ SlidingWindow/SubStringConcatWords.java
@@ -0,0 +1,11 @@
+/*
+You are given a string s and an array of strings words. All the strings of words are of the same length.
+A concatenated string is a string that exactly contains all the strings of any permutation of words concatenated.
+Input: s = "barfoothefoobarman", words = ["foo","bar"]
+
+Output: [0,9]
+Explanation:
+The substring starting at 0 is "barfoo". It is the concatenation of ["bar","foo"] which is a permutation of words.
+The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"] which is a permutation of words.
+*/
+
